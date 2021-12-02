40a202afe14c39cd8445d72e14e493d475451fc4
Konstantinos Liakopoulos <liakopoulos@olympusmarinegroup.com>
2021-12-02 21:07:23 +0200

    products

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React , { Component } from 'react';
+import Products from './Products'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+
+  formatName(user){
+return user.firstName + ' '+user.lastName;
+  }
+
+  render(){
+
+const user = {
+  firstName: 'Kostas',
+  lastName:'Liakopoulos'
 }
 
-export default App;
+
+    return(
+<div>
+  <h1>hello, {this.formatName(user)}</h1>
+  <Products />
+  </div>
+)
+
+
+    }
+}
+
+export default App
---
+++ src/Products.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class Products extends Component{
+
+render() {
+
+const products =  ['Learn React', 'Pro React', 'Beginning React'];
+
+const listProducts = products.map((product) =>
+<li key={product.toString}>{product}</li>)
+
+    return(
+        <div>
+            <ul>{listProducts}</ul>
+            {/* <h2>Products</h2> */}
+            {/* <h2>Courses</h2> */}
+            </div>
+    )
+
+}
+
+}
+
+export default Products;
